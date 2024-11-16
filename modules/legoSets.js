f239e35f79d5e47e6b630f883239f3bdf35b67fd
John Clarence Husenia <clahusenia@gmail.com>
2024-11-15 19:38:55 -0500

    changing adding 1st part

--- modules/legoSets.js
+++ modules/legoSets.js
@@ -6,28 +6,42 @@ class LegoData{
         this.sets = [];
         this.themes = []; 
     }
-    initialize(){
-        return new Promise((resolve,reject)=>{
-        const setData = require("../data/setData");
-        const themeData = require("../data/themeData");
+    initialize() {
+        return new Promise((resolve, reject) => {
+            // Re-read the JSON data from the file
+            fs.readFile('data/setData.json', 'utf8', (err, data) => {
+                if (err) {
+                    reject("Failed to read set data.");
+                    return;
+                }
+    
+                const setData = data ? JSON.parse(data) : [];
+                const themeData = require("../data/themeData");
+    
+                this.themes = [...themeData];
+                this.sets = []; 
+    
 
-        for (let i=0; i<setData.length; i++){
-
-            let setTobeAdded = setData[i]
-            for (let j=0; j<themeData.length; j++){
-            if (setData[i].theme_id == themeData[j].id){
-                setTobeAdded.theme = themeData[j].name
-            }
-        }
-            this.sets.push(setTobeAdded)
-        }
-        if(this.sets != 0){
-            resolve("The sets array is filled with objects.")
-        }else{
-            reject("no data")
-        }
-        
-    });
+                for (let i = 0; i < setData.length; i++) {
+                    let setTobeAdded = setData[i];
+                    let matchingTheme = themeData.find(theme => theme.id === setTobeAdded.theme_id);
+    
+                    if (matchingTheme) {
+                        setTobeAdded.theme = matchingTheme.name;
+                    } else {
+                        setTobeAdded.theme = 'Unknown';
+                    }
+    
+                    this.sets.push(setTobeAdded);
+                }
+    
+                if (this.sets.length > 0) {
+                    resolve("The sets array is filled with objects.");
+                } else {
+                    reject("No data found in setData.");
+                }
+            });
+        });
     }
 
     getAllSets(){
