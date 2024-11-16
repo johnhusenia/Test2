03ea8cf0ccbe8cf422f66fcef5a90f0fb299f2a2
John Clarence Husenia <clahusenia@gmail.com>
2024-11-15 19:19:57 -0500

    adding server

--- server.js
+++ server.js
@@ -32,71 +32,157 @@ app.get("/", (req,res)=>{
 
 });
 
-app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+app.get("/", (req,res)=>{
+    res.render("home");
+
 });
 
-app.get('/insert', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.get('/test', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'test.html'));
+app.get("/about", (req,res)=>{
+    res.render("about");
+
 });
 
-app.get('/lego/sets', async (req, res) => {
-    const theme = req.query.theme;
+app.get("/test", async (req,res)=>{
+    res.render("test1");
+});
+
+
+
+app.get("/lego/addSet",  async (req,res)=>{
+
+    
     try {
-        const themeSets = await legoData.getSetsByTheme(theme);
-        res.json(themeSets);
+        const themeSets = await legoData.getAllThemes();
+        res.render("addSets", {themes: themeSets});
+
     } catch (error) {
         console.error(error);
-        res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+        res.render("404");
     }
+});
+
+app.get("/lego/sets", async (req,res)=>{
+    const theme = req.query.theme;
+    try {
+        const themeSets = await legoData.getSetsByTheme(theme)
+        res.render("sets", {sets: themeSets}); 
+    } catch (error) {
+        console.error(error);
+        res.render("404");
+    }
+    
 });
 
 app.get('/lego/sets/:set_num', async (req, res) => {
     const setNum = req.params.set_num;
     try {
         const setNum1 = await legoData.getSetByNum(setNum);
-        res.json(setNum1);
+        res.render("set", {set: setNum1}); 
     } catch (error) {
         console.error(error);
-        res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+        res.render("404");
     }
 });
 
-app.get('/aboutme', (req, res) => {
-    res.json("Hello I am John Clarence Husenia");
+app.post('/lego/addsets', async (req, res) => {
+    console.log("running add");
+    try {
+        let foundTheme = await legoData.getThemeById(req.body.theme_id);
+        req.body.theme = foundTheme
+        const newObject = req.body;
+        const addset = await legoData.legoaddset(newObject);
+        res.render("set", {set: addset}); 
+    } catch (error) {
+        console.error("Failed to initialize LegoData:", err);
+        res.status(422).send("Failed to initialize data after adding set.");
+    }
+
+
 });
 
-// Endpoint to handle adding an object to JSON file
-app.post('/add-object', (req, res) => {
-    const newObject = req.body;
-
-    fs.readFile('data/setData.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error("Error reading file:", err);
-            return res.status(500).send('Server error');
+app.get('/lego/deleteset/:set_num', async (req, res) => {
+    const theme = req.query.theme;
+    try{ 
+        await legoData.legodeleteset(req.params.set_num); 
+        // const themeSets = await legoData.getSetsByTheme(theme)
+        // res.render("sets", {sets: themeSets}); 
+        res.redirect("/lego/sets"); 
+        }catch(err){ 
+        res.status(404).send(err); 
         }
-
-        let jsonData = data ? JSON.parse(data) : [];
-        jsonData.push(newObject);
-
-        fs.writeFile('data/setData.json', JSON.stringify(jsonData, null, 2), (err) => {
-            if (err) {
-                console.error("Error writing file:", err);
-                return res.status(500).send('Server error');
-            }
-            res.status(200).send('Data added successfully');
-        });
-    });
 });
+
+// next lines will be dealing with files
+
+app.get("/fsets", async (req,res)=>{
+    const theme = req.query.theme;
+    try {
+        const themeSets = await legoData.getSetsByTheme(theme)
+        res.render("fsets", {sets: themeSets}); 
+    } catch (error) {
+        console.error(error);
+        res.render("404");
+    }
+    
+});
+
+app.get("/faddSet",  async (req,res)=>{
+
+    
+    try {
+        const themeSets = await legoData.getAllThemes();
+        res.render("faddSet", {themes: themeSets});
+
+    } catch (error) {
+        console.error(error);
+        res.render("404");
+    }
+});
+
+app.get('/fset/:set_num', async (req, res) => {
+    const setNum = req.params.set_num;
+    try {
+        const setNum1 = await legoData.getSetByNum(setNum);
+        res.render("fset", {set: setNum1}); 
+    } catch (error) {
+        console.error(error);
+        res.render("404");
+    }
+});
+
+app.post('/addsets', async (req, res) => {
+    console.log("running add");
+    try {
+        let foundTheme = await legoData.getThemeById(req.body.theme_id);
+        req.body.theme = foundTheme
+        const newObject = req.body;
+        const addset = await legoData.addsets(newObject);
+        res.render("fset", {set: addset}); 
+    } catch (error) {
+        console.error("Failed to initialize LegoData:", err);
+        res.status(422).send("Failed to initialize data after adding set.");
+    }
+
+
+});
+app.get('/deleteset/:set_num', async (req, res) => {
+    const theme = req.query.theme;
+    try{ 
+        await legoData.deleteSet(req.params.set_num); 
+        // const themeSets = await legoData.getSetsByTheme(theme)
+        // res.render("sets", {sets: themeSets}); 
+        res.redirect("/fsets"); 
+        }catch(err){ 
+        res.status(404).send(err); 
+        }
+});
+
+// this is the end file alteration
 
 // 404 handler for any undefined routes
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.render("404");
 });
-
 
 async function startServer() {
     try {
